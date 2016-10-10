99899d5eae1bd6c2c2e0949311d2c926298a7189
jpeyron <jon.peyron@email.wsu.edu>
2016-10-10 13:15:26 -0700

    zybo: projects:hdmi_in
    updated to work on Vivado 2016.2

--- Projects/hdmi_in/proj/cleanup.cmd
+++ Projects/hdmi_in/proj/cleanup.cmd
@@ -12,10 +12,10 @@ attrib +R .\create_project.tcl
 attrib +R .\cleanup.sh
 attrib +R .\cleanup.cmd
 attrib +R .\.gitignore
+attrib +R .\_READ_ME_.txt
 
 rem delete all non read-only
 del /Q /A:-R .\*
 
 rem unmark read-only
 attrib -R .\*
-
